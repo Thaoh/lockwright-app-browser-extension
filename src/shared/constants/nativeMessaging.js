a1910a8b1e1efc6306b0640a781b69fb4b02f236
Tor-Arne Sandstrak <github@logi.pm>
2026-08-31 13:17:49 +0200

    fix: use lockwright handshake tag

--- src/shared/constants/nativeMessaging.js
+++ src/shared/constants/nativeMessaging.js
@@ -245,5 +245,5 @@ export const PAIRING_REASONS = {
  * These prevent cross-protocol signature replay attacks
  */
 export const PROTOCOL_TAGS = {
-  CLIENT_FINISH: 'pearpass/handshake/v1/clientFinish'
+  CLIENT_FINISH: 'lockwright/handshake/v1/clientFinish'
 }
